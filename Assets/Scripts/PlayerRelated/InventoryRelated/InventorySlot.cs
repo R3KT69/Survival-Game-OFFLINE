07c798c1a581126a07c5857ae56a774f37490945
Asif Ahmed <74340727+R3KT69@users.noreply.github.com>
2026-03-13 10:20:27 +0600

    Inventory slot visualization

--- Assets/Scripts/PlayerRelated/InventoryRelated/InventorySlot.cs
+++ Assets/Scripts/PlayerRelated/InventoryRelated/InventorySlot.cs
@@ -6,11 +6,13 @@ public class InventorySlot : MonoBehaviour
     public Item currentItem;
     public PlayerInventoryManager playerInventoryManager;
     public TextMeshProUGUI UI_text;
+    public TextMeshProUGUI info;
     
 
     void Awake()
     {
         UI_text = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        info = transform.GetChild(1).GetComponent<TextMeshProUGUI>();
 
     }
 
@@ -31,9 +33,11 @@ public class InventorySlot : MonoBehaviour
         if (playerInventoryManager.inv[ind1,ind2] != null)
         {
             UI_text.text = playerInventoryManager.inv[ind1,ind2].id; 
+            info.text = playerInventoryManager.inv[ind1,ind2].runtimeCount.ToString();
         } else
         {
             UI_text.text = "Empty"; 
+            info.text = "0/0";
         }
     }
 
