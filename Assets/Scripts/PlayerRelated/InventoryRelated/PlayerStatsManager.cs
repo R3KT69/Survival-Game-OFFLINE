f1f1a36cff2e6a2c93f574f3c142d1b7a53d4f5e
Asif Ahmed <74340727+R3KT69@users.noreply.github.com>
2026-03-19 22:12:27 +0600

    health_bar + food_bar

--- Assets/Scripts/PlayerRelated/InventoryRelated/PlayerStatsManager.cs
+++ Assets/Scripts/PlayerRelated/InventoryRelated/PlayerStatsManager.cs
@@ -6,12 +6,13 @@ public class PlayerStatsManager : MonoBehaviour
     public PlayerInventoryManager inv;
     public Image health_bar;
     public Image food_bar;
-    public int health;
-    public int food;
+    public int health_pts;
+    public int food_pts;
 
     // Update is called once per frame
     void Update()
     {
-        
+        health_bar.fillAmount = health_pts * 1/100;
+        food_bar.fillAmount = food_pts * 1/100;
     }
 }
