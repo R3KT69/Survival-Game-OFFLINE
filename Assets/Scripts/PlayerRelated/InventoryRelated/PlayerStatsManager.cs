110d40f0eb430c11096a21c5ea723ab12a3b069a
Asif Ahmed <74340727+R3KT69@users.noreply.github.com>
2026-03-19 22:01:30 +0600

    Base of player stats

---
+++ Assets/Scripts/PlayerRelated/InventoryRelated/PlayerStatsManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatsManager : MonoBehaviour
+{
+    public PlayerInventoryManager inv;
+    public Image health_bar;
+    public Image food_bar;
+    public int health;
+    public int food;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
