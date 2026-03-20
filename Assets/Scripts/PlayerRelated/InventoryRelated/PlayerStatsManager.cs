401c625f56bf18e30f33f78735be20f312ccdea3
Asif Ahmed <74340727+R3KT69@users.noreply.github.com>
2026-03-20 07:26:19 +0600

    Added food and health system

--- Assets/Scripts/PlayerRelated/InventoryRelated/PlayerStatsManager.cs
+++ Assets/Scripts/PlayerRelated/InventoryRelated/PlayerStatsManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,8 +17,33 @@ public class PlayerStatsManager : MonoBehaviour
             health_bar = GameObject.Find("health_bar").GetComponent<Image>();
             food_bar = GameObject.Find("food_bar").GetComponent<Image>();
         }
+
+        StartCoroutine(UseFood());
         
     }
+
+    IEnumerator UseFood()
+    {
+        while (true)
+        {
+            if (food_pts > 50)
+            {
+                Debug.Log("Sufficient food");
+                health_pts = Mathf.Min(health_pts + 1, 100);
+                food_pts = Mathf.Max(food_pts - 1, 0);
+            }
+
+            if (food_pts < 10)
+            {
+                Debug.Log("Starving");
+                health_pts = Mathf.Clamp(health_pts - 1, 0, 100);
+            }
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    
 
     void Update()
     {
